89052f65d1b496eb83922ade13b93f5314df7ca2
IgorYudintsev <kakvospitat@gmail.com>
2023-05-25 10:37:12 +0300

    register

--- src/features/auth/Register.tsx
+++ src/features/auth/Register.tsx
@@ -1,21 +1,30 @@
 import { useAppDispatch } from "app/hooks";
 import { authThunks } from "features/auth/auth.slice";
+import { Form, Inputs } from "common/components/Form";
+import { Header } from "common/components/Header";
+import React from "react";
 
 export const Register = () => {
   const dispatch = useAppDispatch();
 
-  const registerHandler = () => {
-    const payload = {
-      email: "dollarselephant1@gmail.com",
-      password: "123456781",
-    };
+  const queryRegister = (payload: Inputs) => {
     dispatch(authThunks.register({ payload }));
   };
 
+  // const registerHandler = () => {
+  //   const payload = {
+  //     email: "dollarselephant2@gmail.com",
+  //     password: "123456782",
+  //   };
+  //   dispatch(authThunks.register({payload}));
+  // };
+
   return (
     <div>
-      <h1>Register</h1>
-      <button onClick={registerHandler}>register</button>
+      <Header />
+      <Form title={"Sign up"} callBack={queryRegister} />
+      {/*<h1>Register</h1>*/}
+      {/*<button onClick={registerHandler}>register</button>*/}
     </div>
   );
 };
